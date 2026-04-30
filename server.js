aea89bf17b211a7b48034e6f31ef74d12f185011
TROGER-VU <ayushgupta91331@gmail.com>
2026-05-01 00:49:59 +0530

    added postgres

--- server.js
+++ server.js
@@ -16,3 +16,15 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const pool = require("./src/config/db");
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "DB connection failed" });
+  }
+});
