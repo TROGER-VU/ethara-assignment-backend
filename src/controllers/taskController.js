f62d73e838fcc240227d668612f09fdb0ab13919
TROGER-VU <ayushgupta91331@gmail.com>
2026-05-01 02:22:42 +0530

    added update task status and get tasks

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,5 +1,26 @@
 const pool = require("../config/db");
 
+// CREATE TASK
+exports.createTask = async (req, res) => {
+  try {
+    const { title, projectId, assignedTo } = req.body;
+    const createdBy = req.user.id;
+
+    const result = await pool.query(
+      `INSERT INTO tasks (title, project_id, assigned_to, created_by)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [title, projectId, assignedTo, createdBy]
+    );
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Create task failed" });
+  }
+};
+
+// ASSIGN TASK
 exports.assignTask = async (req, res) => {
   try {
     const { taskId, userId } = req.body;
@@ -13,5 +34,47 @@ exports.assignTask = async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Assign failed" });
+  }
+};
+
+
+// UPDATE TASK
+exports.updateTaskStatus = async (req, res) => {
+  try {
+    const { taskId, status } = req.body;
+    const userId = req.user.id;
+
+    const result = await pool.query(
+      `UPDATE tasks
+       SET status = $1
+       WHERE id = $2 AND assigned_to = $3
+       RETURNING *`,
+      [status, taskId, userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(403).json({ error: "Not allowed" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Update failed" });
+  }
+};
+
+exports.getProjectTasks = async (req, res) => {
+  try {
+    const { projectId } = req.params;
+
+    const result = await pool.query(
+      `SELECT * FROM tasks WHERE project_id = $1`,
+      [projectId]
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Fetch failed" });
   }
 };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { assignTask } = require("../controllers/taskController");
 const auth = require("../middleware/authMiddleware");
 
-// SIMPLE VERSION (no RBAC yet)
+const { assignTask } = require("../controllers/taskController");
+const { createTask } = require("../controllers/taskController");
+const { updateTaskStatus } = require("../controllers/taskController");
+const { getProjectTasks } = require("../controllers/taskController");
+
+
+
+router.post("/", auth, createTask);
 router.post("/assign", auth, assignTask);
+router.patch("/status", auth, updateTaskStatus);
+router.get("/:projectId", auth, getProjectTasks);
+
+
+
 
 module.exports = router;
