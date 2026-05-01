922c5a081ad4c852b34d9e3fd738899a8bbc7b67
TROGER-VU <ayushgupta91331@gmail.com>
2026-05-01 09:29:50 +0530

    swagger authorize

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,6 +13,15 @@ const options = {
         url: process.env.BASE_URL || "http://localhost:5000",
       },
     ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
     security: [
       {
         bearerAuth: [],
